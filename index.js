5163a14c6198692b115a24edf041c5a41c912fd4
afrozarahmanp <afroza.rahman.p@gmail.com>
2023-08-06 21:59:25 +0600

    recipe and chefdetails route setup

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors');
 const port = process.env.port || 3000;
 
-const data = require('./Data/data.json')
+const recipes = require('./Data/data.json')
 
 app.use(cors())
 
@@ -11,9 +11,21 @@ app.get('/', (req, res) => {
   res.send('Culinary sage server is runnig')
 })
 
-app.get('/data', (req, res) =>{
-res.send(data)
+app.get('/recipes', (req, res) =>{
+res.send(recipes);
 })
+
+app.get('/recipes/:id', (req, res) =>{
+  const id = req.params.id;
+  const selectedFoods = recipes.find(recipe =>recipe.id == id);
+res.send(selectedFoods);
+  })
+app.get('/chefdetails/:id', (req, res) =>{
+  const id = req.params.id;
+  const selectedChef = recipes.find(recipe =>recipe.id == id);
+res.send(selectedChef);
+  })
+
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`)
